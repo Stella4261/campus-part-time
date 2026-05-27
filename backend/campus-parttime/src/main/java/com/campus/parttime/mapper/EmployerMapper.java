2b75215637023885dcb6db26b45dc5a7ad9f103e
Stella4261 <386727341@qq.com>
2026-05-27 11:15:49 +0800

    Delete backend

--- backend/campus-parttime/src/main/java/com/campus/parttime/mapper/EmployerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.campus.parttime.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.campus.parttime.entity.Employer;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface EmployerMapper extends BaseMapper<Employer> {
-}
